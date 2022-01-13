c92ad16f3101447c3d1418fd5ce2efcba78bccc0
Andrew Nosov <andrewnosov@outlook.com>
2022-01-13 14:48:06 +0500

    ADD utils

---
+++ src/utils/formats.js
@@ -0,0 +1,8 @@
+export const formatPrice = (number) => {
+    return new Intl.NumberFormat('ru-RU', {
+        style: 'currency',
+        currency: 'RUB',
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 2,
+    }).format(number);
+}
