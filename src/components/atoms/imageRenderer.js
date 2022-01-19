374f48a59ea70dea67b29893ca38635b9617c9a5
Andrew Nosov <andrewnosov@outlook.com>
2022-01-19 22:17:06 +0500

    ADD image with loading placeholder component

---
+++ src/components/atoms/imageRenderer.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import Image from 'next/image';
+
+export default function ImageRenderer({ src, alt, ...props }) {
+    const [isLoaded, setIsLoaded] = useState(false); // loading state
+
+    return (
+        <>
+            {!isLoaded && <div className='absolute bg-gray-300 rounded-sm'></div>}
+            <Image
+                onLoadingComplete={() => setIsLoaded(true)} // runs when next/image is loaded
+                src={src}
+                alt={alt}
+                {...props} // next/image layout props
+            />
+        </>
+    );
+}
