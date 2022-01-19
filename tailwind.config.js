4a196673beb81704d6d6be2c51be45f83fb148d1
Andrew Nosov <andrewnosov@outlook.com>
2022-01-19 22:15:41 +0500

    ADD custom font/colors/classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,29 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
     content: [
         './src/pages/**/*.{js,ts,jsx,tsx}',
         './src/components/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
-        extend: {},
+        extend: {
+            screens: {}, // custom responsive variables (def sm:640 md:768 lg:1024 xl:1028)
+            colors: {
+                'ivory': 'var(--base-ivory)',
+                'ocean': 'var(--base-ocean)',
+                'miami-pink': 'var(--base-miami-pink)',
+                'palm': 'var(--base-palm)',
+            }, // color variables
+            fontFamily: {
+                primary: [
+                    'Press\\ Start\\ 2P',
+                    ...defaultTheme.fontFamily.sans,
+                ], // pixel or def sans
+            },
+            dropShadow: {
+                'pixel': '10px 10px 0 rgba(0, 0, 0, 0.25)'
+            },
+        },
     },
     plugins: [],
 };
