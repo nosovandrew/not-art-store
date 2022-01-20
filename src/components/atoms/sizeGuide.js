a38259530f2817dc24242c026ac644f7a9114f37
Andrew Nosov <andrewnosov@outlook.com>
2022-01-20 12:01:38 +0500

    ADD ui component for product form (sizes info)

---
+++ src/components/atoms/sizeGuide.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+export default function SizeGuide() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <div className='w-full border-2 border-black flex flex-col'>
+            <button className='px-4 py-2' onClick={() => setIsOpen(true)}>
+                Размерная таблица
+            </button>
+            {isOpen && (
+                <>
+                    <ul className='mt-4 text-center space-y-2'>
+                        <li>S — 25x25см</li>
+                        <li>M — 50x50см</li>
+                        <li>L — 75x75см</li>
+                    </ul>
+                    <button className='mt-2 px-4 py-2' onClick={() => setIsOpen(false)}>Закрыть</button>
+                </>
+            )}
+        </div>
+    );
+}
