9c5d7665f3d5c38bb817713142cec12aa9ca53d1
Andrew Nosov <andrewnosov@outlook.com>
2022-01-20 17:49:30 +0500

    ADD responsiveness

--- src/components/templates/layout.js
+++ src/components/templates/layout.js
@@ -5,7 +5,7 @@ export default function Layout({ children }) {
         <div className='p-4 lg:p-20'>
             <Header />
             <main>{children}</main>
-            <footer className='text-center mt-4 lg:mt-20'>
+            <footer className='flex flex-col items-center justify-center lg:flex-row mt-4 lg:mt-20'>
                 <span className='text-ocean'>ecommerce.</span>
                 <span className='text-palm'>sample.</span>
                 <span className='text-miami-pink'>webnos</span>
