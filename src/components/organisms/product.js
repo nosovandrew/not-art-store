411acd03c0f596d4336bf8a37846828cfd6d9fe6
Andrew Nosov <andrewnosov@outlook.com>
2022-01-20 12:08:16 +0500

    ADD styles and sold out state

--- src/components/organisms/product.js
+++ src/components/organisms/product.js
@@ -1,21 +1,32 @@
-import Image from 'next/image';
-
-import ProductForm from '@/components/molecules/productForm'
+import ProductForm from '@/components/molecules/productForm';
+import ImageRenderer from '@/components/atoms/imageRenderer';
 
 export default function Product({ product }) {
+    const { title, availableForSale } = product;
 
     return (
-        <div>
-            <Image
-                src={product.images.edges[0].node.url}
-                alt={product.images.edges[0].node.altText}
-                layout='fixed'
-                width={300}
-                height={300}
-                quality={95}
-            />
-            <ProductForm product={product} />
+        <div className='w-full flex flex-col items-center'>
+            <div className='w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] m-4 drop-shadow-pixel'>
+                <ImageRenderer
+                    src={product.images.edges[0].node.url}
+                    alt={product.images.edges[0].node.altText}
+                    layout='intrinsic'
+                    width={900}
+                    height={900}
+                    quality={95}
+                />
+            </div>
+            {/* check if product is abailable for sale (in stock) */}
+            {
+                availableForSale ? (
+                    <ProductForm product={product} />
+                ) : (
+                    <div className='flex flex-col items-center m-4 space-y-4'>
+                        <h1>{title}</h1>
+                        <p className='text-red-500 px-4 py-2 border-red-500 border-2'>Нет в наличии</p>
+                    </div>
+                )
+            }
         </div>
     );
 }
-
