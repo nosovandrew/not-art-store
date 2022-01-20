c2921b1f75b03ac54492e0f9c79934bb0c820bd8
Andrew Nosov <andrewnosov@outlook.com>
2022-01-20 12:00:47 +0500

    ADD loading state

--- src/components/atoms/qty.js
+++ src/components/atoms/qty.js
@@ -1,21 +1,21 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 
 import { CartContext } from "@/context/cart/context";
 
 export default function QtyManager({ itemId, itemQty }) {
     // itemId and itemQty are placved in created checkout (local cart)
-    const { increaseItemQty, decreaseItemQty, removeItemFromCart } = useContext(CartContext);
+    const { increaseItemQty, decreaseItemQty, removeItemFromCart, isLoading } = useContext(CartContext);
 
     return (
-        <div>
+        <div className='flex space-x-4'>
             {itemQty > 1 && (
-                <button onClick={() => decreaseItemQty(itemId)}>-</button>
+                <button onClick={() => decreaseItemQty(itemId)} disabled={isLoading ? true : false}>-</button>
             )}
             {itemQty === 1 && (
-                <button onClick={() => removeItemFromCart(itemId)}>x</button>
+                <button onClick={() => removeItemFromCart(itemId)} disabled={isLoading ? true : false}>x</button>
             )}
             <p>{itemQty}</p>
-            <button onClick={() => increaseItemQty(itemId)}>+</button>
+            <button onClick={() => increaseItemQty(itemId)} disabled={isLoading ? true : false}>+</button>
         </div>
     );
 }
